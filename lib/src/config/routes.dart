6bc435be6ee28661296eb6545b5e1117e0ab4a35
ogabek0909 <norpulatovogabek42@gmail.com>
2023-09-08 09:38:06 +0500

    routes with go_router

---
+++ lib/src/config/routes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) =>Container(),
+    ),
+  ],
+);
