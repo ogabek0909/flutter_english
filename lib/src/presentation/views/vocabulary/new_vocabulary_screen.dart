e55cb56721e9f572a97cae4f333f9fa58c1c4f48
ogabek0909 <norpulatovogabek42@gmail.com>
2023-09-09 10:36:30 +0500

    finishing new vocabulary screen

--- lib/src/presentation/views/vocabulary/new_vocabulary_screen.dart
+++ lib/src/presentation/views/vocabulary/new_vocabulary_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:uuid/uuid.dart';
 
 enum Language {
   english,
@@ -18,7 +21,44 @@ class _NewVocabularyScreenState extends State<NewVocabularyScreen> {
   final TextEditingController _english = TextEditingController();
   final TextEditingController _uzbek = TextEditingController();
   final TextEditingController _definition = TextEditingController();
+  bool _waiting = false;
   Language _language = Language.english;
+  var uuid = const Uuid();
+  void _onAdd() async {
+    if (_english.text.isEmpty || _uzbek.text.isEmpty) {
+      return;
+    }
+
+    setState(() {
+      _waiting = true;
+    });
+    try {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .collection('vocabularies')
+          .doc(uuid.v1())
+          .set(
+        {
+          "english": _english.text,
+          "uzbek": _uzbek.text,
+          "definition": _definition.text,
+        },
+      );
+      _english.clear();
+      _uzbek.clear();
+      _definition.clear();
+    } on FirebaseException catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(error.message ?? "Something went wrong!"),
+        ),
+      );
+    }
+    setState(() {
+      _waiting = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,125 +102,129 @@ class _NewVocabularyScreenState extends State<NewVocabularyScreen> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                _language == Language.english ? ' English' : " Uzbek",
-                style: GoogleFonts.foldit(
-                    fontSize: 22, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _english,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        fillColor: Colors.white60,
-                        filled: true,
-                        hintText: 'Word*',
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: const BorderSide(
-                              color: Colors.black,
-                            )),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Text(
-                ' Definition',
-                style: GoogleFonts.foldit(
-                    fontSize: 22, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      maxLines: 5,
-                      controller: _definition,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        fillColor: Colors.white60,
-                        filled: true,
-                        hintText: 'Definition (optional)',
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: const BorderSide(
-                              color: Colors.black,
-                            )),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Text(
-                _language == Language.english ? ' Uzbek' : " English",
-                style: GoogleFonts.foldit(
-                    fontSize: 22, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(height: 5),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _uzbek,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        fillColor: Colors.white60,
-                        filled: true,
-                        hintText: 'Translation*',
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(
-                            color: Colors.black,
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: const BorderSide(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const Spacer(),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(double.infinity, 50)),
-                child: const Text(
-                  'Add To The Vocabularies',
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  _language == Language.english ? ' English' : " Uzbek",
+                  style: GoogleFonts.foldit(
+                      fontSize: 22, fontWeight: FontWeight.w600),
                 ),
-              ),
-            ],
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _english,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          fillColor: Colors.white60,
+                          filled: true,
+                          hintText: 'Word*',
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: const BorderSide(
+                                color: Colors.black,
+                              )),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  ' Definition',
+                  style: GoogleFonts.foldit(
+                      fontSize: 22, fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        maxLines: 5,
+                        controller: _definition,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          fillColor: Colors.white60,
+                          filled: true,
+                          hintText: 'Definition (optional)',
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: const BorderSide(
+                                color: Colors.black,
+                              )),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  _language == Language.english ? ' Uzbek' : " English",
+                  style: GoogleFonts.foldit(
+                      fontSize: 22, fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _uzbek,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          fillColor: Colors.white60,
+                          filled: true,
+                          hintText: 'Translation*',
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: const BorderSide(
+                              color: Colors.black,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 200),
+                _waiting
+                    ? const Center(child: CircularProgressIndicator())
+                    : ElevatedButton(
+                        onPressed: _onAdd,
+                        style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(double.infinity, 50)),
+                        child: const Text(
+                          'Add To The Vocabularies',
+                        ),
+                      ),
+              ],
+            ),
           ),
         ),
       ),
