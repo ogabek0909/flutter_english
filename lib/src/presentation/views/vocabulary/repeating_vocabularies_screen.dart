fd4e1c5912ca9ee08d695334cea4c85386095a07
ogabek0909 <norpulatovogabek42@gmail.com>
2023-09-11 15:46:37 +0500

    new screen for repeating vocabularies

---
+++ lib/src/presentation/views/vocabulary/repeating_vocabularies_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RepeatingVocabulariesScreen extends StatelessWidget {
+  const RepeatingVocabulariesScreen({super.key});
+  static const routeName = 'repeating-vocabularies-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Repeat Your Vocabularies',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      
+    );
+  }
+}
