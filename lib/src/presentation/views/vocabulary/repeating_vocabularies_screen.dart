18cab99c45419107174bfd608c21d7665da9cd50
ogabek0909 <norpulatovogabek42@gmail.com>
2023-09-12 11:23:49 +0500

    selectable text

--- lib/src/presentation/views/vocabulary/repeating_vocabularies_screen.dart
+++ lib/src/presentation/views/vocabulary/repeating_vocabularies_screen.dart
@@ -9,51 +9,53 @@ class RepeatingVocabulariesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Repeat Your Vocabularies',
-            style: TextStyle(color: Colors.black),
+      child: SelectionArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Repeat Your Vocabularies',
+              style: TextStyle(color: Colors.black),
+            ),
           ),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Text(
-                'vocabulary',
-                style: GoogleFonts.akshar(
-                  fontSize: 30,
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Text(
+                  'vocabulary',
+                  style: GoogleFonts.akshar(
+                    fontSize: 30,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                height: 350,
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                decoration: BoxDecoration(
-                  boxShadow: const [
-                    BoxShadow(
-                      offset: Offset(-4, 6),
-                    ),
-                  ],
-                  color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  children: [
-                    SelectableText(
-                      'data',
-                      style: GoogleFonts.akshar(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600,
+                const SizedBox(height: 20),
+                Container(
+                  height: 350,
+                  width: double.infinity,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        offset: Offset(-4, 6),
                       ),
-                      
-                    )
-                  ],
+                    ],
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        'data',
+                        style: GoogleFonts.akshar(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
