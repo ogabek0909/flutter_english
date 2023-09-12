e9fb661265501c79de2dcb5cdff749af7bc62dc1
ogabek0909 <norpulatovogabek42@gmail.com>
2023-09-12 16:12:22 +0500

    adding new cubit

--- lib/app.dart
+++ lib/app.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_english/src/config/routes.dart';
 import 'package:flutter_english/src/config/themes.dart';
-import 'package:flutter_english/src/presentation/blocs/bloc/vocabularies_bloc.dart';
+import 'package:flutter_english/src/presentation/providers/bloc/vocabularies_bloc.dart';
+import 'package:flutter_english/src/presentation/providers/cubit/index_cubit.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => VocabulariesBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => VocabulariesBloc()),
+        BlocProvider(create: (context) => IndexCubit(),),
+      ],
       child: MaterialApp.router(
         theme: AppTheme.light,
         debugShowCheckedModeBanner: false,
