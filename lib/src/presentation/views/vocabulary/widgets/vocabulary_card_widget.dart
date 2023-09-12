a17efbc5cae53a2eebb352b03963cb5110497433
ogabek0909 <norpulatovogabek42@gmail.com>
2023-09-12 16:14:40 +0500

    new widget

---
+++ lib/src/presentation/views/vocabulary/widgets/vocabulary_card_widget.dart
@@ -0,0 +1,176 @@
+import 'package:flip_card/flip_card.dart';
+import 'package:flip_card/flip_card_controller.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_english/src/presentation/providers/bloc/vocabularies_bloc.dart';
+import 'package:flutter_english/src/presentation/providers/cubit/index_cubit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+class VocabularyCardWidget extends StatefulWidget {
+  // final Vocabulary vocabulary;
+  const VocabularyCardWidget({
+    super.key,
+    // required this.vocabulary,
+  });
+
+  @override
+  State<VocabularyCardWidget> createState() => _VocabularyCardWidgetState();
+}
+
+class _VocabularyCardWidgetState extends State<VocabularyCardWidget> {
+  int vocabularyIndex = 0;
+  final FlipCardController _cardController = FlipCardController();
+
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<VocabulariesBloc>(context).add(RepeatingVocabularyEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<VocabulariesBloc, VocabulariesState>(
+      builder: (context, state) => BlocBuilder<IndexCubit, IndexState>(
+        builder: (context, indexState) => FlipCard(
+          controller: _cardController,
+          back: Container(
+            height: 350,
+            width: 400,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            decoration: BoxDecoration(
+              boxShadow: const [
+                BoxShadow(
+                  offset: Offset(4, 6),
+                ),
+              ],
+              color: Colors.blueAccent[100],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'TRANSLATION',
+                  style: GoogleFonts.akshar(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 4,
+                        color: Colors.yellow[100],
+                      ),
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      state.vocabularies[indexState.index].uzbek,
+                      style: GoogleFonts.akshar(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                        color: const Color.fromARGB(247, 210, 128, 6),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        _cardController.toggleCardWithoutAnimation();
+                        if (indexState.index == state.vocabularies.length) {
+                          
+                        }else {
+                          BlocProvider.of<IndexCubit>(context).increment();
+
+                        }
+                        print('object');
+                      },
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(130, 40)),
+                      child: const Text('I don\'t know'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        _cardController.toggleCardWithoutAnimation();
+                        if (indexState.index == state.vocabularies.length) {
+                          
+                        }else{
+                          BlocProvider.of<IndexCubit>(context).increment();
+
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(130, 40)),
+                      child: const Text('I know'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          front: Container(
+            height: 350,
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            decoration: BoxDecoration(
+              boxShadow: const [
+                BoxShadow(
+                  offset: Offset(-4, 6),
+                ),
+              ],
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(
+                    'WORD',
+                    style: GoogleFonts.akshar(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 4,
+                          color: Colors.yellow[100],
+                        ),
+                      )
+                    ],
+                  ),
+                  Text(
+                    state.vocabularies[indexState.index].english,
+                    style: GoogleFonts.akshar(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.indigo[100],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    state.vocabularies[indexState.index].definition,
+                    style: GoogleFonts.alice(
+                      fontSize: 23,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.indigo[100],
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
