93fa0b93d57d4d2f15f955cef9d2b5c4fae3c9b5
Mac <mac@Macs-MacBook-Pro.local>
2024-03-05 10:25:38 +0500

    adding images to main screen

--- lib/src/presentation/views/categories_screen.dart
+++ lib/src/presentation/views/categories_screen.dart
@@ -22,85 +22,112 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Color.fromARGB(255, 11, 23, 80),
         centerTitle: true,
         title: Text(
           'IMPROVE YOUR ENGLISH',
           style: GoogleFonts.abhayaLibre(
             color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 25,
+            // fontWeight: FontWeight.bold,
+            // fontSize: 25,
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  context.goNamed(VocabulariesListScreen.routeName);
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  height:
-                      ((MediaQuery.of(context).size.width - 50) / 2) * (11 / 8),
-                  decoration: BoxDecoration(
-                    color: Colors.lightGreen[400],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: Text(
-                          'English Vocabulary',
-                          style: GoogleFonts.zenAntique(
-                            fontSize: 25,
-                            color: Colors.white,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/astronomy.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(right: 10, left: 10),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    context.goNamed(VocabulariesListScreen.routeName);
+                  },
+                  child: Container(
+                    height: ((MediaQuery.of(context).size.width - 50) / 2) *
+                        (11 / 8),
+                    decoration: BoxDecoration(
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/english.jpeg'),
+                        fit: BoxFit.cover,
+                      ),
+                      color: Colors.lightGreen[400],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        boxShadow: const [
+                          BoxShadow(color: Colors.black12),
+                          BoxShadow(color: Colors.black12),
+                        ],
+                        borderRadius: BorderRadius.circular(10),
+                        // color: Colors.black12,
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Text(
+                              'English Vocabulary',
+                              style: GoogleFonts.zenAntique(
+                                fontSize: 25,
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
-                        ),
+                          Expanded(
+                            child: Container(),
+                          ),
+                        ],
                       ),
-                      Expanded(
-                        child: Container(),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  CategoryWidget(
-                    categoryColor: Colors.teal[400],
-                    categoryName: 'English Books',
-                  ),
-                  const SizedBox(width: 10),
-                  CategoryWidget(
-                    categoryColor: Colors.red[300],
-                    categoryName: 'English Musics',
-                    onTap: () {
-                      context.goNamed(MusicListScreen.routeName);
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  CategoryWidget(
-                    categoryColor: Colors.indigo[400],
-                    categoryName: 'English Movies',
-                  ),
-                  const SizedBox(width: 10),
-                  CategoryWidget(
-                    categoryColor: Colors.purple[300],
-                    categoryName: 'English Podcasts',
-                  ),
-                ],
-              ),
-            ],
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    CategoryWidget(
+                      categoryColor: Colors.teal[400],
+                      categoryName: 'English Books',
+                      imagePath: 'assets/images/book.webp',
+                    ),
+                    const SizedBox(width: 10),
+                    CategoryWidget(
+                      categoryColor: Colors.red[300],
+                      categoryName: 'English Musics',
+                      imagePath: "assets/images/music-note.webp",
+                      onTap: () {
+                        context.goNamed(MusicListScreen.routeName);
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    CategoryWidget(
+                      categoryColor: Colors.indigo[400],
+                      categoryName: 'English Movies',
+                      imagePath: 'assets/images/movie.jpeg',
+                    ),
+                    const SizedBox(width: 10),
+                    CategoryWidget(
+                      categoryColor: Colors.purple[300],
+                      categoryName: 'English Podcasts',
+                      imagePath: 'assets/images/podcast.jpeg',
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -112,11 +139,13 @@ class CategoryWidget extends StatelessWidget {
   final String categoryName;
   final Color? categoryColor;
   final double? categoryWidth;
+  final String imagePath;
   final void Function()? onTap;
   const CategoryWidget({
     super.key,
     required this.categoryColor,
     required this.categoryName,
+    required this.imagePath,
     this.onTap,
     this.categoryWidth,
   });
@@ -131,19 +160,32 @@ class CategoryWidget extends StatelessWidget {
           // width: categoryWidth ?? ((MediaQuery.of(context).size.width - 50) / 2),
           decoration: BoxDecoration(
             color: categoryColor,
+            image: DecorationImage(
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
+            ),
             borderRadius: BorderRadius.circular(10),
           ),
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Text(
-                categoryName,
-                style: GoogleFonts.zenAntique(
-                  fontSize: 25,
-                  color: Colors.white,
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(color: Colors.black12),
+                BoxShadow(color: Colors.black12),
+              ],
+            ),
+            child: Column(
+              children: [
+                Text(
+                  categoryName,
+                  style: GoogleFonts.zenAntique(
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
