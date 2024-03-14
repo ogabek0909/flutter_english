6d1a0c9683c445aa65e5c3d6a650b34950d3ec90
ogabek0909 <norpulatovogabek42@gmail.com>
2024-03-14 11:03:25 +0500

    theming result screen

--- lib/src/presentation/views/vocabulary/result_screen.dart
+++ lib/src/presentation/views/vocabulary/result_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
@@ -6,14 +8,31 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Your Result",
-          style: TextStyle(
-            color: Colors.black,
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/result.jpeg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.white.withOpacity(.06),
+        appBar: AppBar(
+          backgroundColor: Colors.black45,
+          title: const Text(
+            "Your Result",
+          ),
+          leading: IconButton(
+            onPressed: () {
+              context.pop();
+              context.pop();
+            },
+            icon: const Icon(Icons.chevron_left_sharp, size: 30),
           ),
         ),
+        body: Center(
+          
+        )
       ),
     );
   }
